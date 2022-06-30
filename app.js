9c36149eb9b251a8f6e7e4277dab51af128a6781
Adrian Butler Jr <butlerjr123@gmail.com>
2022-06-30 14:14:55 -0500

    added random number guesser

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let colorThemeIconElement;
 let dateElement;
 const sunIconPath = "sun.svg";
-const moonIconPath="moon.svg";
+const moonIconPath = "moon.svg";
+let randomNumberInputElement;
+let randomNumberSubmitElement;
+let randomNumberResultElement;
+
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 document.addEventListener(`DOMContentLoaded`, function()
 {
@@ -10,6 +15,12 @@ document.addEventListener(`DOMContentLoaded`, function()
 
     dateElement = document.getElementById("date-and-time");
     dateElement.innerHTML = new Date();
+
+    randomNumberInputElement = document.getElementById("randomNumberInput");
+    randomNumberSubmitElement = document.getElementById("randomNumberSubmit");
+    randomNumberResultElement = document.getElementById("randomNumberResult");
+
+    randomNumberSubmitElement.onclick = checkInput;
     
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) // toggled dark mode if user has system set to dark mode
     {
@@ -19,6 +30,19 @@ document.addEventListener(`DOMContentLoaded`, function()
 
 }, false);
 
+
+function checkInput()
+{
+    if (randomNumberInputElement.value == randomNumber)
+    {
+        randomNumberResultElement.innerHTML = "You guessed right!"
+    }
+
+    else
+    {
+        randomNumberResultElement.innerHTML = "Nope!"
+    }
+}
 
 function toggleDarkMode()
 {
