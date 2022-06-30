da2e65d346e77206e91d043bfeea3dfd49761b37
Adrian Butler Jr <butlerjr123@gmail.com>
2022-06-29 21:43:29 -0500

    added a clock

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let colorThemeIconElement;
+let dateElement;
 const sunIconPath = "sun.svg";
 const moonIconPath="moon.svg";
 
@@ -6,6 +7,9 @@ document.addEventListener(`DOMContentLoaded`, function()
 {
     colorThemeIconElement = document.getElementById("color-theme-switch");
     colorThemeIconElement.onclick = toggleDarkMode;
+
+    dateElement = document.getElementById("date-and-time");
+    dateElement.innerHTML = new Date();
     
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) // toggled dark mode if user has system set to dark mode
     {
