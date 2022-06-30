c08b2752d60c188de56ea861841489bc3c5e93fa
Adrian Butler Jr <butlerjr123@gmail.com>
2022-06-30 17:46:43 -0500

    added loop demo

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ let colorThemeIconElement;
 let dateElement;
 const sunIconPath = "sun.svg";
 const moonIconPath = "moon.svg";
+
 let randomNumberInputElement;
 let randomNumberSubmitElement;
 let randomNumberResultElement;
-
 const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+let loopDemonstrationElement;
+let loopNumberInputElement;
+let loopSubmitButtonElement;
 
 document.addEventListener(`DOMContentLoaded`, function()
 {
@@ -14,14 +18,19 @@ document.addEventListener(`DOMContentLoaded`, function()
     colorThemeIconElement.onclick = toggleDarkMode;
 
     dateElement = document.getElementById("date-and-time");
-    dateElement.innerHTML = new Date();
+    dateElement.innerHTML = new Date().toString();
 
     randomNumberInputElement = document.getElementById("randomNumberInput");
     randomNumberSubmitElement = document.getElementById("randomNumberSubmit");
     randomNumberResultElement = document.getElementById("randomNumberResult");
-
     randomNumberSubmitElement.onclick = checkInput;
-    
+
+    loopDemonstrationElement = document.getElementById("loop-demonstration");
+    loopNumberInputElement = document.getElementById("loop-number-input");
+    loopSubmitButtonElement = document.getElementById("loop-submit-button");
+
+    loopSubmitButtonElement.onclick = loopDemo;
+
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) // toggled dark mode if user has system set to dark mode
     {
         toggleDarkMode();
@@ -29,6 +38,23 @@ document.addEventListener(`DOMContentLoaded`, function()
 
 
 }, false);
+
+function loopDemo()
+{
+    const timesToLoop = loopNumberInputElement.value;
+
+    for (let i = 0; i < timesToLoop; i++)
+    {
+        let imageElement = document.createElement("img");
+        imageElement.setAttribute("src", "https://placeholder.pics/svg/300");
+
+        let countElement = document.createElement("p");
+        countElement.appendChild(document.createTextNode((i + 1).toString()));
+
+        loopDemonstrationElement.appendChild(countElement);
+        loopDemonstrationElement.appendChild(imageElement)
+    }
+}
 
 
 function checkInput()
